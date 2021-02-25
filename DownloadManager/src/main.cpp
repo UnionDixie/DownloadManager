8dd4dc16eac9e1b253e6ef0dbf57bcd1528a44c1
UnionDixie <60270800+UnionDixie@users.noreply.github.com>
2021-02-25 16:25:03 +0300

    Added Test cURL

--- DownloadManager/src/main.cpp
+++ DownloadManager/src/main.cpp
@@ -1,8 +1,34 @@
 #include "IOControler/iocontoler.h"
 
+#define CURL_STATICLIB
+#pragma warning(disable:4996)
+#include <curl/curl.h>
+
+//size_t downloadfile(void* ptr,size_t size,size_t nmeb,FILE* stream) {
+//	return fwrite(ptr, size, nmeb, stream);
+//}
+
 int main () {
-	IO io;
-	io.run();
+	//IO io;
+	//io.run();
+
+	//CURL* curl;
+	//CURLcode responce;
+	//curl = curl_easy_init();
+	//curl_easy_setopt(curl,CURLOPT_URL,"https://yandex.ru");
+	//responce = curl_easy_perform(curl);
+	//curl_easy_cleanup(curl);
+
+	/*CURL* curl = curl_easy_init();
+	FILE* file = fopen("file.png", "wb");
+	curl_easy_setopt(curl, CURLOPT_URL, "https://yt3.ggpht.com/ytc/AAUvwniKD1_1tlhfun6u-jIlqNXbYURruCAS88clJb4d=s88-c-k-c0x00ffffff-no-rj");
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, downloadfile);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
+
+	CURLcode responce = curl_easy_perform(curl);
+	curl_easy_cleanup(curl);
+	fclose(file);*/
+
 
 	return 0;
 }
